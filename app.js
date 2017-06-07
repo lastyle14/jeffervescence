e475088d563c94c10a3dac6c384f0ebee77e81ae
Lex Anderson <lexanderson1114@gmail.com>
2017-06-07 11:51:26 -0400

    checked correctly

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-console.log('yep')
+const app = {
+    init(formSelector) {
+        document
+            .querySelector(formSelector)
+            .addEventListener('submit', this.addFlick)
+    },
+
+addFlick(ev) {
+    ev.preventDefault()
+    const flickName = ev.target.flickname.value
+    console.log(flickName)
+},
+
+
+}
+
+app.init('#flickForm')
